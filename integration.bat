57ee688c97b64095cb9453f0b2b4435f7dd85d46
Ivan Kott <vanya.platun@yandex.ru>
2023-12-17 22:26:13 +0300

    start test

--- integration.bat
+++ integration.bat
@@ -1,3 +1,1 @@
-[D:\Dev\Java\java-converter\dist\target\classes> java converter/Main]
-
-java converter/Main D:\Dev\Java\java-converter\test\input.txt D:\Dev\Java\java-converter\test\output.txt
+java converter/Main D:\Dev\Java\java-converter\files\input.txt D:\Dev\Java\java-converter\files\output.txt
